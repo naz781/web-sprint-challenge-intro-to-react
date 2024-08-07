b7bcc99b7db466a87b05dd7ae91792515aa97169
Fatima Naz <naz781@gmail.com>
2024-08-07 16:29:53 -0400

    sprint 6 solution

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,23 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
+import {useState} from 'react'
+
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
+  
+  const [characters, setCharacters] = useState([])
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [peopleResponse, planetsResponse] = await Promise.all([
+          axios.get(urlPeople),
+          axios.get(urlPlanets)
+        ]);
+
+        const people = peopleResponse.data
+        console.log(people)
+        const planets = planetsResponse.data
+        console.log(planets)
+
+        const characterWithPlanets = people.map(person => {
+          const homeworld = planets.find(planet => planet.id === person.homeworld);
+          return { ...person,homeworld};
+        })
+        setCharacters(characterWithPlanets);
+      
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    fetchData();
+  }, [])
+
+  
   return (
     <div>
       <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      <div className='characters-list'>
+      {characters.map((character) => (
+        <Character key={character.id} character={character} />
+      ))}
+    </div>
     </div>
   )
 }
 
 export default App
 
-// ❗ DO NOT CHANGE THE CODE  BELOW
+
 if (typeof module !== 'undefined' && module.exports) module.exports = App
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function Character() { // ❗ Add the props
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  // ❗ Create a "toggle" click handler to show or remove the homeworld
+
+function Character({ character }) { 
+  
+  const[showHomeworld, setShowHomeworld] = useState(false)
+
+  const toggleHomeworld = () => {
+    setShowHomeworld(!showHomeworld)
+}
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className='character-card'onClick={toggleHomeworld}>
+    <h3 className="character-name"> {character.name}</h3>
+    <p >Gender: {character.gender}</p>
+    <p >Birth Year: {character.birth_year}</p>
+    <p >Created: {character.created}</p>
+    <p >Edited: {character.edited}</p>
+    <p >Eye Color: {character.eye_color}</p>
+    <p >Height: {character.height}</p>
+    <p >Skin Color: {character.skin_color}</p>
+    <p >Mass: {character.mass}</p>
+    
+      {showHomeworld && (
+        <div className='character-planet'>
+          <h3>Homeworld: {character.homeworld.name}</h3>
+          <p>Climate: {character.homeworld.climate}</p>
+          <p>Created: {character.homeworld.created}</p>
+          <p>Diameter: {character.homeworld.diameter}</p>
+          <p>Edited: {character.homeworld.edited}</p>
+          <p>Gravity: {character.homeworld.gravity}</p>
+          <p>Orbital Period: {character.homeworld.orbital_period}</p>
+          <p>Population: {character.homeworld.population}</p>
+          <p>Rotation Period: {character.homeworld.rotation_period}</p>
+          <p>Surface Water: {character.homeworld.surface_water}</p>
+          <p>Terrain: {character.homeworld.terrain}</p>
+        </div>
+      )
+      }
     </div>
   )
 }
